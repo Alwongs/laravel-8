aa614f40fa4502d9ef56da14be6bc3b1e103f647
Alexander <alwong@ya.ru>
2024-03-24 12:35:51 +0400

    update

---
+++ database/migrations/2024_03_24_082730_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id('event_id');
+            $table->string('event');
+            $table->string('description');
+            $table->timestamp('timestamp');
+            $table->string('type');
+            $table->string('user_id');
+          
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
