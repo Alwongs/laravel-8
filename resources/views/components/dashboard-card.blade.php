446eb0dc4d222ea817ac7c07fe0e9ff1e477c30a
Alexander <alwong@ya.ru>
2024-03-26 10:09:25 +0400

    update

--- resources/views/components/dashboard-card.blade.php
+++ resources/views/components/dashboard-card.blade.php
@@ -1,1 +1,25 @@
-<h1>Dashboard-card</h1>
+
+<div class="dashboard-card">
+    <h2 class="dashboard-card__title">Dashboard-card</h2>
+
+        <ul class="dashboard-card__content-list">
+
+                <li class="dashboard-card__item dashboard-card-item">
+
+                    <a class="dashboard-card-item__title" href="#" title="">hhhh</a>
+
+                    <div class="dashboard-card-item__btn-block"> 
+                        <a href=""  class="cell-btn btn-icon-edit"></a>
+
+                            <a href="" class="cell-btn btn-icon-delete"></a>
+
+                            <a href="" class="cell-btn btn-icon-postpone"></a>
+
+                    </div>
+                </li>
+
+        </ul>
+
+        <p class="dashboard-card__content-empty">-- cписок пуст --</p>
+
+</div>
