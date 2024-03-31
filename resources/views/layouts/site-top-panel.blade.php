26c59c9c12197930bd84fad1b9a6b4235b007dcf
Alexander <alwong@ya.ru>
2024-03-31 16:06:19 +0400

    update

--- resources/views/layouts/site-top-panel.blade.php
+++ resources/views/layouts/site-top-panel.blade.php
@@ -1,15 +1,15 @@
 <header class="top-panel">
-    <div class="">
+    <div class="top-panel__logo">
         <a href="/">Главная</a>
     </div>   
     
-    <nav class="">
+    <nav class="top-panel__navigation">
         <a href="/dashboard">Dashboard</a> 
         <a href="/events">Events</a> 
         <a href="/posts">Posts</a> 
     </nav>
     
-    <div class="">
+    <div class="top-panel__auth">
         @auth
             <a href="#">{{ Auth::user()->email }}</a>
         @else
