decbd8ec5e672ef3c24a9f04422e9f6580021467
Alexander <alwong@ya.ru>
2024-03-31 21:38:28 +0400

    started blog page

--- resources/views/pages/site/blog.blade.php
+++ resources/views/pages/site/blog.blade.php
@@ -1,18 +1,33 @@
 <x-site-layout>
     <section class="section section-banner">
         <img src="{{ Storage::url('/site/main.jpg') }}" alt="main image" />
-        <h2 class="home-title">Ulyanovsk</h2>
+        <h2 class="home-title">Blog</h2>
     </section>
 
-
-    <h1>Blog</h1>
-
-    @isset($posts)
-        <ul>
-            @foreach($posts as $post)
-                <li>{{ $post->post }}</li>
-            @endforeach
-        </ul>
-    @endisset
+    <main class="main">
+        @isset($posts)
+            <ul class="blog-list">
+                @foreach($posts as $post)
+                    <li class="post-page-item">
+                        <div class="post-page-item__image">
+                            @if ($post->image)
+                                <img src="{{ Storage::url($post->image) }}" alt="" title="" />
+                            @else
+                                <img src="/images/default-post-image.jpg" alt="" >
+                            @endif
+                        </div>
+                        <div class="post-page-item__text">
+                            <h3 class="post-page-item__title">{{ $post->post }}</h3>
+                            <p class="post-page-item__description">{{ $post->description }}</p>
+                            <div class="post-page-item__footer">
+                                <span class="post-page-item__date">{{ $post->timestamp }} {{ $post->user_id }}</span>
+                                <a href="/">Read more</a>
+                            </div>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        @endisset
+    </main>
 
 </x-site-layout>
