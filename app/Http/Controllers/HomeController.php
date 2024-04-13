5fa864fb6963df03c959b2395652485201250897
Alexander <alwong@ya.ru>
2024-04-13 18:37:07 +0400

    add visits

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Visit;
 
 class HomeController extends Controller
 {
     public function index() {
 
-        $userInfo = '(' .date("d.m.Y h:s"). ') user IP: ' .$_SERVER['REMOTE_ADDR']; // to save in db
+        // $userInfo = '(' .date("d.m.Y h:s"). ') user IP: ' .$_SERVER['REMOTE_ADDR']; 
+
+        $visit['ip_address'] = $_SERVER['REMOTE_ADDR'];
+        $visit['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
+
+        Visit::create($visit);
 
         $posts = Post::orderBy('id', 'desc')->take(3)->get();
 
