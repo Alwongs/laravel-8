f8be3a9177ea6788415660a02039342115422111
Alexander <alwong@ya.ru>
2024-04-26 09:09:32 +0400

    add robots.txt

--- resources/views/layouts/site-top-panel.blade.php
+++ resources/views/layouts/site-top-panel.blade.php
@@ -1,20 +1,23 @@
 <header class="top-panel">
-    <a class="top-panel__home-link" href="/">{{ __("home") }}</a>  
-    <a id="top-panel-menu-link" class="top-panel__menu-link">Menu</a>
-    
-    <nav class="top-panel__navigation">
-        <a href="/dashboard">Dashboard</a> 
-        <a href="/events">Events</a> 
-        <a href="{{ url('blog') }}">Blog</a> 
-        <a href="#">Gallery</a> 
-    </nav>
-    
-    <div class="top-panel__auth">
-        @auth
-            <a href="#">{{ Auth::user()->name }}</a>
-        @else
-            <a href="{{ route('login') }}">Login</a>   
-            <!-- <a href="{{ route('register') }}" class="">Register</a> -->
-        @endauth
+    <div class="container top-panel__container">
+        <a class="top-panel__home-link" href="/">{{ __("home") }}</a>  
+        <a id="top-panel-menu-link" class="top-panel__menu-link">Menu</a>
+        
+        <nav class="top-panel__navigation">
+            @auth
+                <a href="/dashboard">Dashboard</a> 
+            @endauth
+            <a href="{{ url('blog') }}">Blog</a> 
+            <a href="#">Gallery</a> 
+        </nav>
+        
+        <div class="top-panel__auth">
+            @auth
+                <a href="#">{{ Auth::user()->name }}</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>   
+                <!-- <a href="{{ route('register') }}" class="">Register</a> -->
+            @endauth
+        </div>
     </div>
 </header>
